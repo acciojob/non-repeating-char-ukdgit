e8a520c5754ad6903d75827de7964b63b40f6f53
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-05-01 20:01:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+function firstNonRepeatedChar(str) {
+  let charCount = {};
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]]) {
+      charCount[str[i]]++;
+    } else {
+      charCount[str[i]] = 1;
+    }
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]] === 1) {
+      return str[i];
+    }
+  }
+  return null;
+}
+
+const input = prompt("Enter a string");
+alert(firstNonRepeatedChar(input)); //"abcabcdbca" - d
